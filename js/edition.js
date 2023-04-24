712d7815eae3ed962cc7a3ac15310cd7b019c1b7
anthonyruvoen <anthonyruvoen@gmail.com>
2023-04-24 11:39:36 +0200

    ajout image

--- js/edition.js
+++ js/edition.js
@@ -1,5 +1,12 @@
 const workedit = document.querySelector('.edition');
+const bgedit = document.querySelector('.bgedit');
+const addwork = document.querySelector('.addwork');
+const addphoto = document.querySelector('#addphotobutton');
+const newimg = document.querySelector('.newimg')
+const imgdisplay = document.querySelector('#img');
+const emptyimg = document.querySelector('.emptyimg');
 
+// Ouverture et fermeture de la modale d'édition principale
 
 modifier1.addEventListener('click', () => {
         workedit.classList.add("active");
@@ -8,6 +15,12 @@ modifier1.addEventListener('click', () => {
 closeedit.addEventListener('click', () => {
         workedit.classList.remove("active");
 });
+bgedit.addEventListener('click', () => {
+        workedit.classList.remove("active");
+});
+
+
+// Récupération et affichage des travaux sur la page d'édition
 
 const container3 = document.querySelector('.works');
 const getWorks2 = () => {
@@ -25,19 +38,69 @@ const getWorks2 = () => {
             <div class="deletework"><i class="fa-solid fa-trash-can"></i></div>
             <figcaption>éditer</figcaption>
             </figure>`
-            // var workimg = document.querySelector('#workimg'+data[work].id);
-            // var movework = document.querySelector('#movework i');
-            // console.log(movework);
+        }       
 
-            // workimg.addEventListener('mouseover', () => {
-            //     movework.classList.add("active");
-            // });
-            // workimg.addEventListener('mouseout', () => {
-            //     movework.classList.remove("active");
-            // });
-        }        
-        
+//Suppression d'un travail au clic sur la poubelle
+
+    const deleteWork = document.querySelector('.deletework i');
+            
+            deleteWork.addEventListener('click', (e) => {
+                console.log(data.id);
+            });
     })
 }
 
 getWorks2()
+
+
+// ouverture et fermeture de la page d'ajout de projet
+
+addphoto.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addwork.classList.add("active");
+    workedit.classList.remove("active");
+});
+back.addEventListener('click', () => {
+    addwork.classList.remove("active");
+    workedit.classList.add("active");
+});
+closeedit2.addEventListener('click', () => {
+    addwork.classList.remove("active");
+    workedit.classList.remove("active");
+    newimg.style.display = 'none';
+    emptyimg.style.display = 'block';
+    file.value = '';
+});
+
+
+
+
+//Ajout d'un nouveau projet/photo
+
+const file = document.querySelector('#file');
+
+file.addEventListener('change', previewFile);
+
+function previewFile() {
+
+    const fileregex = /\.(jpe?g|png)$/i;
+
+    if (this.files.length === 0 || !fileregex.test(this.files[0].name)) {
+        return;
+    }
+    const newfile = this.files[0];
+    const newfileReader = new FileReader();
+    newfileReader.readAsDataURL(newfile);
+    newfileReader.addEventListener('load', (e) => displayImage(e, file))
+}
+
+displayImage = (e, file) => {
+    
+    imgdisplay.src = e.target.result;
+    newimg.appendChild(imgdisplay);
+    newimg.style.display = 'flex';
+    emptyimg.style.display = 'none';
+    
+
+
+}
